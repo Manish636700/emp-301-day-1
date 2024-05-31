9f2547788ddcc5255ef81760c46451dd76332499
Manish Mittal <mittalmanish881@gmail.com>
2024-05-31 13:40:43 +0530

    day1

---
+++ src/Day_1/Polymorphism/Method_Overloading.java
@@ -0,0 +1,26 @@
+package Day_1.Polymorphism;
+
+class A{
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    public void sum()
+    {
+        int sum1 = a + b;
+        System.out.println(sum1);
+    }
+    public void multi()
+    {
+        int sum1 = a * b * c;
+        System.out.println(sum1);
+    }
+
+}
+
+public class Method_Overloading {
+    public static void main(String[] args) {
+        A a = new A();
+        a.sum();
+        a.multi();
+    }
+}
